f3accf3e87c9e5ac3c67ec86a4d31170fb805f36
Brandon Myers <bmyers@mozilla.com>
2017-05-23 18:18:45 -0500

    Add logic to throw exception if no config

--- tests/footyhints/test_config.py
+++ tests/footyhints/test_config.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from footyhints.config import Config
 
 
@@ -21,3 +23,10 @@ class TestSample2Config(object):
 
     def test_web_debug(self):
         assert self.config.web_debug is False
+
+
+class TestNonexistentConfig(object):
+    def test_bad_location(self):
+        with raises(RuntimeError) as exception_obj:
+            Config(location="../tests/footyhints/example_configs/abcd.txt")
+        assert 'Unable to find config file at' in str(exception_obj.value)
