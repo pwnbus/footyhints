a59a82e19354260d8c83584370b0e41d5da96820
Brandon Myers <bmyers@mozilla.com>
2017-05-22 23:17:15 -0500

    Add basic exception handling for game scores

--- footyhints/models/game.py
+++ footyhints/models/game.py
@@ -10,3 +10,19 @@ class Game(object):
 
         self.home_team = home_team
         self.away_team = away_team
+        self.home_score = None
+        self.away_score = None
+
+    def worth_watching(self):
+        if self.home_score is None and self.away_score is None:
+            raise ValueError('Score must be defined for both the home and away team')
+        elif self.home_score is None:
+            raise ValueError('Score must be defined for the home team')
+        elif self.away_score is None:
+            raise ValueError('Score must be defined for the away team')
+
+        # Check if home team score exists
+        # Check if away team score exists
+
+        # Main decision consists of:
+        #   If score is nil nil, not worth watching
--- tests/footyhints/models/test_game.py
+++ tests/footyhints/models/test_game.py
@@ -25,3 +25,32 @@ class TestGameInit(object):
         with pytest.raises(ValueError) as exception_obj:
             Game(home_team=team1, away_team=team2)
         assert str(exception_obj.value) == 'away_team must be of type "Team"'
+
+
+class TestGameWorthWatching(object):
+    def setup(self):
+        self.home_team = Team()
+        self.away_team = Team()
+        self.game = Game(home_team=self.home_team, away_team=self.away_team)
+
+    def test_no_home_away_scores(self):
+        with pytest.raises(ValueError) as exception_obj:
+            self.game.worth_watching()
+        assert str(exception_obj.value) == 'Score must be defined for both the home and away team'
+
+    def test_no_home_score(self):
+        self.game.away_score = 3
+        with pytest.raises(ValueError) as exception_obj:
+            self.game.worth_watching()
+        assert str(exception_obj.value) == 'Score must be defined for the home team'
+
+    def test_no_away_score(self):
+        self.game.home_score = 2
+        with pytest.raises(ValueError) as exception_obj:
+            self.game.worth_watching()
+        assert str(exception_obj.value) == 'Score must be defined for the away team'
+
+        # THis should error
+
+    # def test_0_0_game(self):
+
