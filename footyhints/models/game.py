e1ac87ce10703d29e5fe54818d75917027d1b0f6
Brandon Myers <bmyers@mozilla.com>
2017-05-24 02:06:56 -0500

    Add bootstrap files

--- footyhints/models/game.py
+++ footyhints/models/game.py
@@ -16,8 +16,8 @@ class Game(Base):
     home_team = relationship("Team", foreign_keys=[home_team_id], backref='home_games')
     away_team_id = Column(Integer, ForeignKey('teams.id'), nullable=False)
     away_team = relationship("Team", foreign_keys=[away_team_id], backref='away_games')
-    home_score = Column(Integer, nullable=True)
-    away_score = Column(Integer, nullable=True)
+    home_team_score = Column(Integer, nullable=True)
+    away_team_score = Column(Integer, nullable=True)
 
     def __init__(self, home_team, away_team):
         if not type(home_team) is Team:
@@ -49,11 +49,11 @@ class Game(Base):
         elif type(away_team_score) is not int:
             raise TypeError('Away team score must be an integer')
 
-        self.home_score = home_team_score
-        self.away_score = away_team_score
+        self.home_team_score = home_team_score
+        self.away_team_score = away_team_score
 
     def worth_watching(self):
-        if self.home_score is None and self.away_score is None:
+        if self.home_team_score is None and self.away_team_score is None:
             raise TypeError('Home and away scores must be set')
 
         # Main decision logic
