80b7ef993eff63da19ab14e1ef14ed6a8fc372b8
Brandon Myers <bmyers@mozilla.com>
2017-05-24 00:50:35 -0500

    Add more logic to total score plugin

--- footyhints/plugins/total_score.py
+++ footyhints/plugins/total_score.py
@@ -5,8 +5,14 @@ from footyhints.plugin import Plugin
 class HighScore(Plugin):
     def score(self):
         total_goals = self.game.home_score + self.game.away_score
-        if total_goals > 3:
+        if total_goals > 6:
             return 10
+        elif total_goals > 4:
+            return 7
+        elif total_goals > 3:
+            return 3
+        elif total_goals > 2:
+            return 2
         elif total_goals > 1:
             return 0
         else:
