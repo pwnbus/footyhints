866097e26e769c2073ea99cd919eb451cba41c90
Brandon Myers <bmyers@mozilla.com>
2017-05-24 22:31:26 -0500

    Add indentation for total score plugin

--- footyhints/plugins/total_score.py
+++ footyhints/plugins/total_score.py
@@ -5,6 +5,6 @@ class HighScore(Plugin):
     def score(self):
         total_goals = self.game.home_team_score + self.game.away_team_score
         if total_goals == 0:
-          return -10
+            return -10
         score_per_goal = 15
         return total_goals * score_per_goal
