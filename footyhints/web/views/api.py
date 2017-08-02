6da5113535c2832ad5e0731103acde20e456c720
Brandon Myers <bmyers@mozilla.com>
2017-08-02 00:15:19 -0500

    Add team view

--- footyhints/web/views/api.py
+++ footyhints/web/views/api.py
@@ -6,14 +6,6 @@ from footyhints.models.team import Team
 mod = Blueprint('api', __name__)
 
 
-@mod.route('/api/games/all')
-def api_games_all():
-    data = []
-    games = session.query(Game).all()
-    data = generate_games_data(games)
-    return jsonify(data=data)
-
-
 def generate_games_data(games):
     data = []
     for game in games:
@@ -23,14 +15,24 @@ def generate_games_data(games):
 
         data.append(dict(
             game_id=game.id,
-            home_team=game.home_team.name,
-            away_team=game.away_team.name,
+            home_team_name=game.home_team.name,
+            home_team_id=game.home_team.id,
+            away_team_name=game.away_team.name,
+            away_team_id=game.away_team.id,
             round_num=game.round.num,
             interest_level=game.interest_level,
             interest_score=game.interest_score,
             score_modifications=score_modifications
         ))
     return data
+
+
+@mod.route('/api/games/all')
+def api_games_all():
+    data = []
+    games = session.query(Game).all()
+    data = generate_games_data(games)
+    return jsonify(data=data)
 
 
 @mod.route('/api/team/<id>/games')
