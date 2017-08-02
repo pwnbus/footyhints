b036be96fed1b470aa0f5c4c3edec3025b281bb0
Brandon Myers <bmyers@mozilla.com>
2017-08-02 00:19:27 -0500

    Improve specs for api routes

--- tests/footyhints/web/test_api.py
+++ tests/footyhints/web/test_api.py
@@ -11,11 +11,7 @@ class TestApi(WebTest):
         self.session.add(self.game)
         self.session.commit()
 
-    def test_api_games(self):
-        self.game.worth_watching()
-        self.session.add(self.game)
-        self.session.commit()
-        games_resp = self.client.get('/api/games')
+    def verify_games_resp(self, games_resp):
         table_obj = loads(games_resp.data.decode('utf8'))
         assert len(table_obj) == 1
         row_obj = table_obj['data'][0]
@@ -23,9 +19,25 @@ class TestApi(WebTest):
         assert type(row_obj['interest_level']) is str
         assert row_obj['round_num'] == 1
         assert row_obj['game_id'] == 1
-        assert row_obj['home_team'] == 'Chelsea'
-        assert row_obj['away_team'] == 'Manchester United'
+        assert row_obj['home_team_name'] == 'Chelsea'
+        assert row_obj['home_team_id'] == 1
+        assert row_obj['away_team_name'] == 'Manchester United'
+        assert row_obj['away_team_id'] == 2
         assert len(row_obj['score_modifications']) > 0
         for score_modification in row_obj['score_modifications']:
             assert score_modification[0] >= 0
             assert type(score_modification[1]) is str
+
+    def test_api_games_all(self):
+        self.game.worth_watching()
+        self.session.add(self.game)
+        self.session.commit()
+        games_resp = self.client.get('/api/games/all')
+        self.verify_games_resp(games_resp)
+
+    def test_api_team_games(self):
+        self.game.worth_watching()
+        self.session.add(self.game)
+        self.session.commit()
+        games_resp = self.client.get('/api/team/1/games')
+        self.verify_games_resp(games_resp)
