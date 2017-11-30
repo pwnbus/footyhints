5643a7b5ade4342610e4058240ba4e63f11c397b
Brandon Myers <bmyers@mozilla.com>
2017-11-30 17:38:39 -0600

    Fix config location and delete db in setup script

--- bin/setup_db.py
+++ bin/setup_db.py
@@ -2,6 +2,11 @@ from footyhints.config import config
 
 from import_pl_data import import_production_data
 from import_sample_data import import_development_data
+from footyhints.db import create_db, delete_db
+
+print("Setting up DB")
+delete_db()
+create_db()
 
 if config.mode == 'production':
     import_production_data()
