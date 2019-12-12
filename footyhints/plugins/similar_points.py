d6457cb86167864963b14e7e03c4edd675042420
Brandon Myers <bmyers@mozilla.com>
2019-12-12 02:20:31 -0600

    Convert similar points plugin to look at points during specific matchday

--- footyhints/plugins/similar_points.py
+++ footyhints/plugins/similar_points.py
@@ -1,13 +1,30 @@
 from footyhints.plugin import Plugin
 
 
+def points_at_matchday(team, matchday):
+    total_points = 0
+    for game in team.games:
+        if game.round.num < matchday:
+            if game.home_team == team:
+                if game.home_team_score > game.away_team_score:
+                    total_points += 3
+                elif game.home_team_score == game.away_team_score:
+                    total_points += 1
+            elif game.away_team == team:
+                if game.away_team_score > game.home_team_score:
+                    total_points += 3
+                elif game.away_team_score == game.home_team_score:
+                    total_points += 1
+    return total_points
+
+
 class SimilarPoints(Plugin):
     description = 'Proximity in points'
     priority = 2
 
     def score(self):
-        home_points = self.game.home_team.points
-        away_points = self.game.away_team.points
+        home_points = points_at_matchday(self.game.home_team, self.game.round.num)
+        away_points = points_at_matchday(self.game.away_team, self.game.round.num)
         difference = abs(home_points - away_points)
         if difference == 0:
             return 100, "Same number of points in table"
