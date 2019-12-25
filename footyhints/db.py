5a83054f96c51a4e1e51be12353920d1440e3afc
Brandon Myers <bmyers@mozilla.com>
2019-12-24 23:42:30 -0600

    Set pool_recycle to 0

--- footyhints/db.py
+++ footyhints/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from footyhints.config import config
 from footyhints.models.base import Base
 
-engine = create_engine(config.db_uri)
+engine = create_engine(config.db_uri, pool_recycle=0)
 session = scoped_session(sessionmaker(bind=engine))
 
 
