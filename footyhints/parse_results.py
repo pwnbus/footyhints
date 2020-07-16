3ef7e876133f21358f975a748a3471e7d0d3b8e5
Brandon Myers <bmyers@mozilla.com>
2020-07-16 18:24:50 -0500

    Add flag to update db

--- footyhints/parse_results.py
+++ footyhints/parse_results.py
@@ -6,12 +6,19 @@ from footyhints.decision_maker import DecisionMaker
 
 
 class ParseResults():
+    def __init__(self, update=False):
+        self.update = update
 
     def create_teams(self, results):
+        teams = {}
+        if self.update:
+            db_teams = session.query(Team).all()
+            for team in db_teams:
+                teams[team.name] = team
+            return teams
         home_teams = [result['home_team'] for result in results]
         away_teams = [result['away_team'] for result in results]
         team_names = set(home_teams + away_teams)
-        teams = {}
         for team_name in team_names:
             team = Team(name=team_name)
             teams[team_name] = team
@@ -23,6 +30,11 @@ class ParseResults():
         teams = self.create_teams(results)
         decision_maker = DecisionMaker()
         for match in results:
+            if self.update:
+                home_team = session.query(Team).filter(Team.name == match['home_team']).one()
+                game_found = session.query(Game).filter(Game.match_day == match['match_day']).filter(Game.home_team == home_team).all()
+                if game_found:
+                    continue
             game = Game(
                 home_team=teams[match['home_team']],
                 away_team=teams[match['away_team']],
