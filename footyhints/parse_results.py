95c3ee191561909e10e4a6aa15a84f7ddf5eb1a0
Brandon Myers <bmyers@mozilla.com>
2020-07-16 17:17:53 -0500

    Analyze game by game and then commit

--- footyhints/parse_results.py
+++ footyhints/parse_results.py
@@ -7,13 +7,8 @@ from footyhints.decision_maker import DecisionMaker
 
 
 class ParseResults():
-    def determine_worth_watching(self, games):
-        decision_maker = DecisionMaker()
-        for game in games:
-            decision_maker.worth_watching(game)
-            session.add(game)
 
-    def parse_results(self, results):
+    def create_teams(self, results):
         home_teams = [result['home_team'] for result in results]
         away_teams = [result['away_team'] for result in results]
         team_names = set(home_teams + away_teams)
@@ -23,7 +18,11 @@ class ParseResults():
             teams[team_name] = team
             session.add(team)
         session.commit()
-        games = []
+        return teams
+
+    def parse_results(self, results):
+        teams = self.create_teams(results)
+        decision_maker = DecisionMaker()
         for match in results:
             round_obj = Round(match['match_day'])
             session.add(round_obj)
@@ -41,8 +40,7 @@ class ParseResults():
                 game.away_team_score,
                 round_obj.num
             ))
-            games.append(game)
-
-        self.determine_worth_watching(games)
-        session.commit()
+            decision_maker.worth_watching(game)
+            session.add(game)
+            session.commit()
         session.close()
