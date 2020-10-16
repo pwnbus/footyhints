e2d413a4913a8c4afd38b92db5f8fa68069bd4e6
Brandon Myers <bmyers@mozilla.com>
2020-10-15 22:40:29 -0500

    Add version info dynamically

--- web/views.py
+++ web/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from web.models import Team, Competition, Game
 
@@ -5,10 +6,13 @@ from web.models import Team, Competition, Game
 def load_defaults():
     teams = Team.objects.all().order_by('name')
     competition = Competition.objects.all()[0]
+    version = 'v-dev'
+    if 'FOOTYHINTS_VERSION' in os.environ:
+        version = os.environ['FOOTYHINTS_VERSION']
     return {
         "teams": teams,
         "league": competition.name,
-        "version": "v-dev",
+        "version": version,
         "last_updated": competition.last_updated,
     }
 
