ab62378cd793a53930cd3bca499b5be55f841e42
Brandon Myers <pwnbus@users.noreply.github.com>
2020-10-19 00:13:48 -0500

    Improve release script (#125)

--- .do-release.sh
+++ .do-release.sh
@@ -11,8 +11,24 @@ PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
 # Increase version
 PATCH=$((PATCH+1))
 
-# Create new tag
 NEW_TAG="$MAJOR.$MINOR.$PATCH"
-echo "Updating to $NEW_TAG"
-git tag $NEW_TAG
-git push origin $NEW_TAG
+
+# Check to see if:
+# 1. There are any local modifications
+# 2. Local is not up to date with remote master
+if [ -z "$(git diff origin/master)" ]; then
+  continue
+else
+  echo "There are files either local or local is missing commits from remote...you need to manually fix"
+  exit 1
+fi
+
+# Prompt user to create new tag
+while true; do
+    read -p "Tag to create: $NEW_TAG (y/n)? " answer
+    case $answer in
+        [Yy]* ) echo "Updating to $NEW_TAG"; git tag $NEW_TAG; git push origin $NEW_TAG; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
