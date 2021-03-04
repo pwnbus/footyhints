0479113a4d0b0edecbc1ce87d7f1ec1a9749bf7d
Brandon Myers <bmyers@mozilla.com>
2021-03-04 00:44:48 -0600

    Convert high profile matchup to use team.place

--- footyhints/plugins/high_profile_matchup.py
+++ footyhints/plugins/high_profile_matchup.py
@@ -1,21 +1,26 @@
 from footyhints.plugin import Plugin
 
 
-HIGH_PROFILE_TEAMS = [
-    "Arsenal",
-    "Chelsea",
-    "Liverpool",
-    "Manchester United",
-    "Manchester City",
-    "Tottenham",
-]
-
-
 class HighProfileMatchup(Plugin):
     priority = 1
+    required_num_games = 3
 
     def score(self, game):
-        if game.home_team.name in HIGH_PROFILE_TEAMS and game.away_team.name in HIGH_PROFILE_TEAMS:
-            return 100, "Big matchup"
+        if game.home_team.played < self.required_num_games or game.away_team.played < self.required_num_games:
+            return 0, "Too Early in Season"
+
+        if game.home_team.place <= 6 and game.away_team.place <= 6:
+            return 100, "Top 6 matchup ({} v {})".format(game.home_team.place, game.away_team.place)
+
+        if game.home_team.place >= 15 and game.away_team.place >= 15:
+            return 100, "Fight for relegation battle ({} v {})".format(game.home_team.place, game.away_team.place)
+
+        place_delta = abs(game.home_team.place - game.away_team.place)
+        if place_delta <= 3:
+            return 100, "Within 3 places in the table ({} v {})".format(game.home_team.place, game.away_team.place)
+        elif place_delta <= 6:
+            return 50, "Within 6 places in the table ({} v {})".format(game.home_team.place, game.away_team.place)
+        elif place_delta <= 9:
+            return 10, "Within 9 places in the table ({} v {})".format(game.home_team.place, game.away_team.place)
         else:
-            return 0, "Not a big matchup"
+            return -50, "Not a big game ({} v {})".format(game.home_team.place, game.away_team.place)
