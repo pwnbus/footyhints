06009379db8f15c9f5dcdfcd21686b1e7c05b2f7
Brandon Myers <pwnbus@users.noreply.github.com>
2021-05-12 23:22:33 -0500

    Add logger line for mode (#210)

--- web/management/commands/setupdb.py
+++ web/management/commands/setupdb.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
+
 from footyhints.sample_data_client import SampleDataClient
 from footyhints.data_client import DataClient
 from footyhints.parse_results import ParseResults
 from footyhints.config import config
+from footyhints.logger import logger
 
 
 class Command(BaseCommand):
@@ -18,6 +20,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        logger.info("Running in mode: {}".format(config.mode))
         if config.mode == 'production':
             client = DataClient()
         else:
