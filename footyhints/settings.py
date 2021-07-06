3254bea1956a645754b7fc69a10f87b241a9eeda
Brandon Myers <bmyers@mozilla.com>
2021-07-06 17:55:15 -0500

    Remove google analytics

--- footyhints/settings.py
+++ footyhints/settings.py
@@ -52,7 +52,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'web.context_processors.google_analytics',
             ],
         },
     },
