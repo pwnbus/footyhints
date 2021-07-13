e92e59f0dc104aa965f7b67a2ad042360e0e7b98
Brandon Myers <bmyers@mozilla.com>
2021-07-12 23:12:39 -0500

    Add ability to load templates if mobile

--- web/views.py
+++ web/views.py
@@ -4,8 +4,10 @@ from django.views.decorators.cache import cache_page
 from web.models import Team, Competition
 from footyhints.config import config
 
+from .helpers import mobile_browser
 
-def load_defaults():
+
+def load_defaults(request):
     teams = Team.objects.all().order_by('name')
     competition = Competition.objects.all()[0]
     version = 'v-dev'
@@ -16,12 +18,13 @@ def load_defaults():
         "competition": competition,
         "version": version,
         "last_updated": competition.last_updated,
+        "mobile": mobile_browser(request)
     }
 
 
 @cache_page(config.cache_expiration)
 def index(request):
-    context = load_defaults()
+    context = load_defaults(request)
     return render(request, 'index.html', context)
 
 
@@ -31,12 +34,12 @@ def team(request, team_id):
         team = Team.objects.get(pk=team_id)
     except Team.DoesNotExist:
         return redirect('/')
-    context = load_defaults()
+    context = load_defaults(request)
     context['team'] = team
     return render(request, 'team.html', context)
 
 
 @cache_page(config.cache_expiration)
 def table(request):
-    context = load_defaults()
+    context = load_defaults(request)
     return render(request, 'table.html', context)
