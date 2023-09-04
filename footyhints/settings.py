0d7db0397e155c8aa223a5f18d499df031aae17e
Brandon Myers <pwnbus@users.noreply.github.com>
2023-09-04 16:49:00 -0500

    Split out production tasks (#283)
    * Split out production tasks
    
    * Store datetime as isoformat and parse in javascript
    
    * Stop running containers on deploy

--- footyhints/settings.py
+++ footyhints/settings.py
@@ -98,12 +98,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# Comment this out to make dates work correctly in web ui
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- tests/footyhints/test_config_parser.py
+++ tests/footyhints/test_config_parser.py
@@ -15,6 +15,7 @@ class NoEnvironment():
         self.delete_env_key("FOOTYHINTS_MODE")
         self.delete_env_key("FOOTYHINTS_API_KEY")
         self.delete_env_key("FOOTYHINTS_WEB_DEBUG")
+        self.delete_env_key("FOOTYHINTS_LEAGUE_ID")
         self.delete_env_key("FOOTYHINTS_DB_URI")
         self.delete_env_key("FOOTYHINTS_CACHE_ENABLED")
         self.delete_env_key("FOOTYHINTS_CACHE_URI")
@@ -34,6 +35,9 @@ class TestSample1Config(NoEnvironment):
 
     def test_api_key(self):
         assert self.config.api_key is None
+
+    def test_league_id(self):
+        assert self.config.league_id == 123
 
     def test_db_uri(self):
         assert self.config.db_uri == "sqlite:///:memory:"
@@ -55,6 +59,9 @@ class TestSample2Config(NoEnvironment):
 
     def test_api_key(self):
         assert self.config.api_key == '12345678'
+
+    def test_league_id(self):
+        assert self.config.league_id == 123
 
     def test_db_uri(self):
         assert self.config.db_uri == "mysql://user:pass@127.0.0.1/footyhints"
@@ -84,6 +91,7 @@ class TestSampleEnvironmentConfig():
         os.environ["FOOTYHINTS_MODE"] = "production"
         os.environ["FOOTYHINTS_API_KEY"] = "12345678"
         os.environ["FOOTYHINTS_WEB_DEBUG"] = "False"
+        os.environ["FOOTYHINTS_LEAGUE_ID"] = "123"
         os.environ["FOOTYHINTS_DB_URI"] = "sqlite:///:memory:"
         self.config = ConfigParser()
 
