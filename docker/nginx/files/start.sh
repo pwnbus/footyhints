fa1343d1f40b949f7287e17858dcaff7bb75892c
Brandon Myers <brandon@brandon-myers.com>
2024-04-07 23:32:22 -0500

    Add letsencrypt container

--- docker/nginx/files/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp /etc/nginx/ssl/preloaded/localhost.crt /etc/nginx/ssl/live/localhost/localhost.crt
-cp /etc/nginx/ssl/preloaded/localhost.key /etc/nginx/ssl/live/localhost/localhost.key
-
-# Start nginx
-/usr/sbin/nginx
